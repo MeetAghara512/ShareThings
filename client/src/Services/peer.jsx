7d7a85507d2fdbec8cce3bb8dce7484b250483a4
MeetAghara512 <meet.aghara522@gmail.com>
2025-06-11 11:48:13 +0530

    running screen sharing

--- client/src/Services/peer.jsx
+++ client/src/Services/peer.jsx
@@ -1,40 +1,47 @@
-class peerService {
-      constructor() {
-            if (!this.peer) {
-                  this.peer = new RTCPeerConnection({
-                        iceServers: [{
-                              urls: [
-                                    "stun:stun.l.google.com:19302", //open servers 
-                                    "stun:global.stun.twilio.com:3478",
-                              ],
-                        }]
-                  })
-            }
-      }
+class PeerService {
+  constructor() {
+    if (!this.peer) {
+      this.peer = new RTCPeerConnection({
+        iceServers: [
+          {
+            urls: [
+              "stun:stun.l.google.com:19302",
+              "stun:global.stun.twilio.com:3478",
+            ],
+          },
+        ],
+      });
+    }
+  }
 
-      async getAnswer(offer){
-            if(this.peer){
-                  await this.peer.setRemoteDescription(offer);
-                  const ans = await this.peer.createAnswer();
-                  await this.peer.setLocalDescription(new RTCSessionDescription(ans));
-                  return ans;
-            }
-      }
+  async getAnswer(offer) {
+    if (this.peer) {
+      await this.peer.setRemoteDescription(new RTCSessionDescription(offer));
+      const ans = await this.peer.createAnswer();
+      await this.peer.setLocalDescription(ans);
+      return ans;
+    }
+  }
 
-      
-      async getOffer(){
-            if(this.peer){
-                  const offer = await this.peer.createOffer();
-                  await this.peer.setLocalDescription(new RTCSessionDescription(offer));
-                  return offer;
-                  
-            }
-      }
-      async setLocalDescription(ans){    //setRemoteAnswer(ans)
-            if(this.peer){
-                  await this.peer.setRemoteDescription(new RTCSessionDescription(ans));
-            }
-      }
+  async getOffer() {
+    if (this.peer) {
+      const offer = await this.peer.createOffer();
+      await this.peer.setLocalDescription(offer);
+      return offer;
+    }
+  }
+
+  async setLocalDescription(sdp) {
+    if (this.peer) {
+      await this.peer.setLocalDescription(new RTCSessionDescription(sdp));
+    }
+  }
+
+  async setRemoteDescription(sdp) {
+    if (this.peer) {
+      await this.peer.setRemoteDescription(new RTCSessionDescription(sdp));
+    }
+  }
 }
 
-export default new peerService();
+export default new PeerService(); //main
